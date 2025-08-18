1a81b75b726b47740e316a1daa6008a7cc5621fe
VictorF3671 <victorhugonoronhas@gmail.com>
2025-08-18 00:33:54 -0400

    fixed

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'sonner';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: 'https://financeplus-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
